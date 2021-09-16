14a369d437a89ff4251bb109b48c46a44ea595ec
Luke <l.prylowski@phsolutions.pl>
2021-09-16 12:16:23 +0200

    New update script.

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ echo "Update cypherTrap..."
 cp getTrap.sh /etc/snmp/
 chmod +x /etc/snmp/getTrap.sh
 
-if -f /etc/snmp/getTrap.sh; then
+if [ -f /etc/snmp/getTrap.sh ]; then
 echo "Update is done..,"
 else
 echo "Something went wrong. Please run update script once again..."
