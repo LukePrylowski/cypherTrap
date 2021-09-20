cbbf4dc1022ae6972f2aa1cae787d36ade021419
Luke <l.prylowski@phsolutions.pl>
2021-09-20 17:19:43 +0200

    New version of cypherTrap. Possible to add new model by config file placed in conf.d/

--- getTrap.sh
+++ getTrap.sh
@@ -14,6 +14,7 @@ wifi="NetWiFi"
 sn=""
 net24=""
 net50=""
+cmtsStateMib=".1.3.6.1.4.1.20858.10.22.2.1.1.1."
 
 function getDate()
 {
@@ -21,23 +22,6 @@ function getDate()
     echo ${now}
 }
 
-function setWiFi()
-{
-    local i=$1
-    local n=$2
-    local nt=$3
-    local p=$4
-    local t=$5
-
-    if [[ $t == "TC7230"* ]] || [[ ${type} == "TC7210"* ]]; then
-    
-	snmpset -v2c -c ${comm} ${i} .1.3.6.1.4.1.2863.205.30.1.1.2.1.1.2.${n} i: 1
-	snmpset -v2c -c ${comm} ${i} .1.3.6.1.4.1.2863.205.30.1.1.2.1.1.3.${n} s: "${nt}"
-        snmpset -v2c -c ${comm} ${i} .1.3.6.1.4.1.2863.205.30.1.1.2.1.1.4.${n} i:7
-	snmpset -v2c -c ${comm} ${i} .1.3.6.1.4.1.2863.205.30.1.1.2.3.4.1.2.${n} s: "${p}"
-        snmpset -v2c -c ${comm} ${i} .1.3.6.1.4.1.2863.205.30.1.1.1.1.0 i: 1
-    fi;
-}
 
 function getSn()
 {
@@ -60,7 +44,7 @@ do
 	mac=`echo ${val} | xargs | awk '{print($1$2$3$4$5$6)}'`
     fi;
 
-    if [[ ${oid} == ".1.3.6.1.4.1.20858.10.22.2.1.1.1."* ]]; then
+    if [[ ${oid} == ${cmtsStatusMib}* ]]; then
 
         state=`echo ${val} | xargs` 
     fi;
@@ -68,61 +52,56 @@ do
 done
 
 if [ ${state} -eq 8 ] && [[ ${ip} != "0.0.0.0" ]] && [[ ${mac} != "000000000000" ]]; then
+
     type=`snmpget -v2c -c ${comm} -Onqv $ip .1.3.6.1.2.1.1.1.0 | awk -F 'MODEL: ' '{print($2)}' | sed 's/>>\"//g'`
 
+    path=`pwd`
 
-    if [[ ${type} != "" ]]; then
+    if [[ ${type} != "" ]] && [ -f ${path}/conf.d/${type} ]; then
 
-	if [[ ${type} == "TC7230"* ]] || [[ ${type} == "TC7210"* ]]; then
+	. ${path}/conf.d/${type}
 
-	    net24=`snmpget -Onqv -v2c -c ${comm} $ip .1.3.6.1.4.1.2863.205.30.1.1.2.1.1.3.32 | sed 's/\"//g'`
-	    net50=`snmpget -Onqv -v2c -c ${comm} $ip .1.3.6.1.4.1.2863.205.30.1.1.2.1.1.3.112 | sed 's/\"//g'`
-	else
+	if [ ${#setMibs[@]} -gt 0 ]; then
+
+    	    echo "${now}: CM ${ip} ${mac} ${type} setting online MIBs" >> ${log}
 	    
-	    net24=""
-	    net50=""
+	    for i in "${!onlineMibs[@]}"; do
+	        snmpset -v2c -c ${comm} $ip "${onlineMibs[i]}" "${onlineType[i]}": "${onlineVals[i]}"
+	    done
 	fi;
-    
-	if [[ ${net24} != "" ]]; then
 
-	    if [[ ${net24} != ${wifi}_${mac: -4} ]]; then
+	if [ ${#checkMibs[@]} -gt 0 ]; then
+	    
+	    for i in "${!checkMibs[@]}"; do
+		
+	        tmp=`snmpget -Onqv -v2c -c ${comm} $ip ${checkMibs[i]} | sed 's/\"//g'`
 
-		now=$(getDate)
-    		echo "${now}: CM ${ip} ${mac} ${type} wrong WiFi 2.4GHz ${net24}" >> ${log}
-        	echo "${now}: CM ${ip} ${mac} ${type} getting SN..." >> ${log}
-		getSn ${ip}
-    		echo "${now}: CM ${ip} ${mac} ${type} Wifi 2.4GHz is being updated" >> ${log}
-    		setWiFi ${ip} 32 "${wifi}_${mac: -4}" "${sn}" "${type}"
-	    else
+		if [[ ${tmp} != "" ]] && [[ ${tmp} != ${checkVals[i]} ]]; then
 
-		now=$(getDate)
-    		echo "${now}: CM ${ip} ${mac} ${type} WiFi 2.4GHz is OK" >> ${log}
-	    fi;
-	fi;
-	
-	if [[ ${net50} != "" ]]; then
+		    IFS="|"
 
-	    if [[ ${net50} != ${wifi}_5G_${mac: -4} ]]; then
+		    mibs=${checkMibs[i]}
+		    types=${checkType[i]}
+		    values=${checkVals[i]}
 
-		now=$(getDate)
-    		echo "${now}: CM ${ip} ${mac} ${type} wrong WiFi 5GHz ${net50}" >> ${log}
-    		echo "${now}: CM ${ip} ${mac} ${type} getting SN..." >> ${log}
-    		getSn ${ip}
-    		echo "${now}: CM ${ip} ${mac} ${type} Wifi 5GHz is being updated" >> ${log}
-    		setWiFi ${ip} 112 "${wifi}_5G_${mac: -4}" "${sn}" "${type}"
-	    else
+		    now=$(getDate)
+    		    echo "${now}: CM ${ip} ${mac} ${type} wrong ${checkMibs[i]}, current: ${tmp}, expected: ${checkVals[i]}" >> ${log}
+        	    echo "${now}: CM ${ip} ${mac} ${type} getting SN..." >> ${log}
+		    getSn ${ip}
+    		    echo "${now}: CM ${ip} ${mac} ${type} ${checkMibs[i]} is being updated" >> ${log}
 
-		now=$(getDate)
-    		echo "${now}: CM ${ip} ${mac} ${type} WiFi 5GHz is OK" >> ${log}
-	    fi;
-	fi;
-	
-	if [[ ${net24} == "" ]] && [[ ${net50} == "" ]]; then
-
-	    now=$(getDate)
-	    echo "${now}: CM ${ip} ${mac} ${type} not checking..." >> ${log}
-	fi;
+		    
+		    for mib in "${!mibss[@]}"; do
+			snmpset -v2c -c ${comm} ${ip} ${mibs[mib]} ${types[mib]}: ${values[mib]}
+		    done;
+		else
+		    now=$(getDate)
+    		    echo "${now}: CM ${ip} ${mac} ${type} ${checkMibs[i]} is OK" >> ${log}
+		fi;
+	    done;
+	fi;	
+    else    
+        now=$(getDate)
+        echo "${now}: CM ${ip} ${mac} ${type} not checking..." >> ${log}
     fi;
 fi;
-
-
